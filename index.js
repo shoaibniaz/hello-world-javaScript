9e95321701e1fe47f72059341ed04ed0b979787a
shoaibniaz <shaoibahmed7@gmail.com>
2024-02-05 20:51:43 +0500

    javaScript basic parctive

---
+++ index.js
@@ -0,0 +1,113 @@
+// let firstName ="Shoaib"
+// let lastName ="Ahmed"
+// let age ="23"
+// let qualification ="Intermediate"
+// let maritalState ="Single"
+
+// console.log("My First Name is",firstName)
+// console.log("My Last Name is",lastName);
+// console.log("My age is",age);
+// console.log("My qualification is",qualification);
+// console.log("My marital State is",maritalState);
+
+// let myName ="15"
+// console.log("myName",myName);
+// myName=12;
+// sum= myName+10
+// console.log("myName+sum"+myName+10);
+// let a="5"
+// let b="6"
+// let result= "a++ - b-- * b \ a--"
+// console.log("result",result);
+// console.log("a",a);
+// console.log("b",b);
+// console.log("result",result);
+// let Shoaib = "10"
+// Shoaib= 10+10
+// console.log("shoaib",Shoaib);
+// let shoaib= "40"
+// console.log("shoaib",shoaib);
+// let  = " 50 + 50"
+// console.log("shoaib",shoaib);
+
+
+
+// const submitForm = function (event) {
+//     console.log("event  ===========>", event);
+//     event.preventDefault()
+//     const input1 = document.getElementById('1ageInput')
+//     const input2 = document.getElementById('2ageInput')
+
+//     const result = +input1?.value + +input2?.value
+
+//     console.log('Addition ======>', result);
+
+//     const resultDiv = document.getElementById('result')
+//     resultDiv.innerHTML = `<h1> ${result} </h1>`
+// }
+
+
+// function submit(event) {
+//     event.preventDefault()
+//     console.log("----------------- IN Submit =----------------------");
+//     let ageInput1 = document.querySelector("#1ageInput").value;
+//     let ageInput2 = document.querySelector("#2ageInput").value;
+//     console.log("1ageInput", ageInput1);
+//     console.log("2ageInput", ageInpu2);
+//     let result = ageInput1 + ageInput2
+//     console.log("result", result);
+// }
+
+// const login = function (event) {
+//     event.preventDefault()
+//     const email = document.getElementById("email").value;
+//     const password = document.getElementById("password").value;
+//     console.log("email", email);
+//     console.log("password", password);
+
+//     const resultDiv = document.getElementById("result")
+//     resultDiv.innerHTML = `<h1> ${email}</h1> `
+//     resultDiv.innerHTML += `<h1> ${password}</h1>`
+
+// }
+//   form Parctirc
+// const login = function (event) {
+//     event.preventDefault()
+//     const name = document.getElementById("name").value
+//     const password = document.getElementById("password").value
+//     const age = document.getElementById("age").value
+//     const qualification = document.getElementById("qualification").value
+//     console.log("name", name);
+//     console.log("password", password);
+//     console.log("age", age);
+//     console.log("qualification", qualification);
+//     const resultDiv = document.getElementById("result")
+//     resultDiv.innerHTML = ` <h1>${name}</h1>`
+//     resultDiv.innerHTML += ` <h1>${password}</h1>`
+//     resultDiv.innerHTML += ` <h1>${age}</h1>`
+//     resultDiv.innerHTML += ` <h1>${qualification}</h1>`
+
+
+// }
+// }
+// const login = function (event) {
+    //     event.preventDefault()
+    //     const password = document.getElementById("password").value
+    //     const name = document.getElementById("name").value
+    //     const resultDiv = document.getElementById("resultDiv")
+    //     console.log("password",password);
+    //     console.log("name",name )
+    //     resultDiv.innerHTML = `<h1>${password}</h1> <h1>${name}</h1>`
+    // }
+    const login = function (event) {
+            event.preventDefault()
+            const inputPercantage1 = document.getElementById("inputPercantage1").value
+            const inputPercantage2 = document.getElementById("inputPercantage2").value
+            const inputPercantage3 = document.getElementById("inputPercantage3").value
+            const inputPercantage4 = document.getElementById("inputPercantage4").value 
+            console.log("inputPercantage1",inputPercantage1);
+            console.log("inputPercantage2",inputPercantage2);
+            console.log("inputPercantage3",inputPercantage3);
+            console.log("inputPercantage4",inputPercantage4);
+               
+    }
